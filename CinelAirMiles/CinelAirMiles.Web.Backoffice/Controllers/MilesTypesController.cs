aecc97059c3abd2b14f664fe382072882a4a971a
Andreia Correia <amrcorreia@users.noreply.github.com>
2020-10-17 16:39:50 +0100

    MilesTypes and ProgramTiers Controllers

---
+++ CinelAirMiles/CinelAirMiles.Web.Backoffice/Controllers/MilesTypesController.cs
@@ -0,0 +1,158 @@
+﻿using CinelAirMiles.Common.Entities;
+using CinelAirMiles.Web.Backoffice.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinelAirMiles.Web.Backoffice.Controllers
+{
+    public class MilesTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MilesTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: MilesTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MilesTypes.ToListAsync());
+        }
+
+
+        // GET: MilesTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var milesType = await _context.MilesTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (milesType == null)
+            {
+                return NotFound();
+            }
+
+            return View(milesType);
+        }
+
+
+        // GET: MilesTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: MilesTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MilesType milesType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(milesType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(milesType);
+        }
+
+
+        // GET: MilesTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var milesType = await _context.MilesTypes.FindAsync(id);
+            if (milesType == null)
+            {
+                return NotFound();
+            }
+            return View(milesType);
+        }
+
+
+        // POST: MilesTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, MilesType milesType)
+        {
+            if (id != milesType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(milesType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MilesTypeExists(milesType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(milesType);
+        }
+
+        // GET: MilesTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var milesTypes = await _context.MilesTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (milesTypes == null)
+            {
+                return NotFound();
+            }
+
+            return View(milesTypes);
+        }
+
+        // POST: MilesTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var milesTypes = await _context.MilesTypes.FindAsync(id);
+            _context.MilesTypes.Remove(milesTypes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MilesTypeExists(int id)
+        {
+            return _context.MilesTypes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CinelAirMiles/CinelAirMiles.Web.Backoffice/Controllers/ProgramTiersController.cs
@@ -0,0 +1,159 @@
+﻿using CinelAirMiles.Common.Entities;
+using CinelAirMiles.Web.Backoffice.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinelAirMiles.Web.Backoffice.Controllers
+{
+    public class ProgramTiersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProgramTiersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: ProgramTiers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProgramTiers.ToListAsync());
+        }
+
+
+        // GET: ProgramTiers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var programTier = await _context.ProgramTiers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (programTier == null)
+            {
+                return NotFound();
+            }
+
+            return View(programTier);
+        }
+
+
+        // GET: ProgramTiers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: ProgramTiers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProgramTier programTier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(programTier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(programTier);
+        }
+
+
+        // GET: ProgramTiers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var programTier = await _context.ProgramTiers.FindAsync(id);
+            if (programTier == null)
+            {
+                return NotFound();
+            }
+            return View(programTier);
+        }
+
+
+        // POST: ProgramTiers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProgramTier programTier)
+        {
+            if (id != programTier.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(programTier);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProgramTiersExists(programTier.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(programTier);
+        }
+
+        // GET: ProgramTiers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var programTier = await _context.ProgramTiers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (programTier == null)
+            {
+                return NotFound();
+            }
+
+            return View(programTier);
+        }
+
+        // POST: ProgramTiers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var programTier = await _context.ProgramTiers.FindAsync(id);
+            _context.ProgramTiers.Remove(programTier);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private bool ProgramTiersExists(int id)
+        {
+            return _context.ProgramTiers.Any(e => e.Id == id);
+        }
+    }
+}
